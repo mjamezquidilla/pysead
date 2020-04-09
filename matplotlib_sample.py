5fb20cac40ee3933570e061427d6d0191c6d1484
Michael James C. Quidilla <mjamezquidilla@gmail.com>
2020-04-10 01:43:48 +0800

    Changes in commit

--- matplotlib_sample.py
+++ matplotlib_sample.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
+plt.style.use('fivethirtyeight')
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
 
-x = [0, 1]
+x = [0, 10]
 y = [2, 3]
-z = [4, 5]
+z = [3, 5]
 
-ax.plot(x,y,z)
+ax.plot(x,y,z, marker = 'o', linewidth = 1)
 plt.show()
+
+
+
+
+
