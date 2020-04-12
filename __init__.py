b3131dabd74f522f35d56e67289b6fffd09b6750
Michael James C. Quidilla <mjamezquidilla@gmail.com>
2020-04-12 20:14:59 +0800

    Fixed Init

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from pysead.Truss_2D import Truss_2D
 from pysead.Truss_3D import Truss_3D
-from pysead.Frame_2D import Frame_2D
+from pysead import Frame_2D
 
 # print("Positive Values for forces: right, up (righthand rule)")
 # print("Negative moment = clockwise, Positive moment = counter-clockwise (righthand rule)")
